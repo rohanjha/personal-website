8c26cfd4f024362ea9f2974ace61c6e4c27470f4
Rohan Jha <rohanjha@Rohans-Mac-mini.local>
2017-08-12 16:20:31 -0700

    many changes

--- js/main.js
+++ js/main.js
@@ -1,27 +1,8 @@
 let $leftButton = $("#left-button");
 let $rightButton = $("#right-button");
-let $title = $("#title");
-let $body = $("body");
-let $content = $("#content");
-let $html = $("html");
 
 let index = 0;
-let titles = [
-  "<u><a href=\"https://wikispeedia.herokuapp.com\" id=\"title-link\">Wikispeedia</a></u>",
-  "Maps",
-  "CubeSAT",
-  "<u><a href=\"https://rohanjha.github.io\" id=\"title-link\">Hubway Maps</a></u>",
-  "Music Visuals",
-  "<u><a href=\"http://rpubs.com/rohan_jha/polarization\" id=\"title-link\">Polarization Study</a></u>"];
-
-let backgroundColors = ["#cb356b", "#bd3f32"];
-
-let r1 = 255;
-let g1 = 81;
-let b1 = 47;
-let r2 = 221;
-let g2 = 36;
-let b2 = 118;
+let titleIds = ["wikispeedia-title", "maps-title", "cubesat-title", "hubway-title", "music-title", "polarization-title"];
 
 $(document).ready(() => {
   updateContent();
@@ -38,27 +19,24 @@ $rightButton.on("click", () => {
 });
 
 function updateContent() {
-  let ra = Math.floor(r1 + (r2 - r1) * (1 / titles.length) * (index + 1.7));
-  let ga = Math.floor(g1 + (g2 - g1) * (1 / titles.length) * (index + 1.7));
-  let ba = Math.floor(b1 + (b2 - b1) * (1 / titles.length) * (index + 1.7));
-
-  let rb = Math.floor(r1 + (r2 - r1) * (1 / titles.length) * index);
-  let gb = Math.floor(g1 + (g2 - g1) * (1 / titles.length) * index);
-  let bb = Math.floor(b1 + (b2 - b1) * (1 / titles.length) * index);
-
-  document.body.style.backgroundImage = `linear-gradient(to left, rgb(${ra}, ${ga}, ${ba}) , rgb(${rb}, ${gb}, ${bb}))`;
-
   if (index == 0) {
     $leftButton.prop('disabled', true);
   } else {
     $leftButton.prop('disabled', false);
   }
 
-  if (index == titles.length - 1) {
+  if (index == titleIds.length - 1) {
     $rightButton.prop('disabled', true);
   } else {
     $rightButton.prop('disabled', false);
   }
 
-  $title.html(`<b>${titles[index]}</b> (${index + 1} of ${titles.length})`);
+  for (let i = 0; i < titleIds.length; i++) {
+    $title = $("#" + titleIds[i]);
+    if (i != index) {
+      $title.hide();
+    } else {
+      $title.show();
+    }
+  }
 }
